4d9e0c391aba7827a5ee72a5e7d8c88fbcb595fd
harrison <harrisonpinchieri@gmail.com>
2023-12-17 18:35:46 -0300

    Users app

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -21,7 +21,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("projects.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("projects/", include("projects.urls")),
+    path("", include("users.urls")),
+]
 
 # images
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
