9ca72292205b2fa4e7751f356f50009645de5eed
harrison <harrisonpinchieri@gmail.com>
2023-12-19 16:30:53 -0300

    signals

--- users/models.py
+++ users/models.py
@@ -3,6 +3,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+from django.db.models.signals import (
+    post_save,
+    post_delete,
+)  # triga quando um model salva
+
+from django.dispatch import receiver
+
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -29,7 +37,7 @@ class Profile(models.Model):
     )
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 
 class Skill(models.Model):
@@ -43,3 +51,26 @@ class Skill(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+# @receiver(post_save, sender=Profile)
+# def createProfile(sender, instance, created, **kwargs):
+#    if created:
+#        user = instance
+#        profile = Profile.objects.create(
+#            user=user,
+#            username=user.username,
+#            email=user.email,
+#            name=user.first_name,
+#        )
+#
+#
+# def deleteUser(sender, instance, **kwargs):
+#    user = instance.user
+#    user.delete()
+#    print("Deleting user...")
+#
+#
+# post_save.connect(createProfile, sender=User)
+# post_delete.connect(deleteUser, sender=Profile)
+####
