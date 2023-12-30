454edcae2a8f650ebbcc24fb376a4853c409bc09
harrison <harrisonpinchieri@gmail.com>
2023-12-30 13:30:13 -0300

    pagination

--- projects/views.py
+++ projects/views.py
@@ -4,7 +4,8 @@ from .models import Project, Tag
 from .forms import ProjectForm
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from .utils import searchProjects
+from .utils import searchProjects, paginateProjects
+
 
 # Create your views here.
 
@@ -12,7 +13,13 @@ from .utils import searchProjects
 def projects(request):
     projects, search_query = searchProjects(request)
 
-    context = {"projects": projects, "search_query": search_query}
+    custom_range, projects = paginateProjects(request, projects, 2)
+
+    context = {
+        "projects": projects,
+        "search_query": search_query,
+        "custom_range": custom_range,
+    }
     return render(request, "projects/projects.html", context)
 
 
