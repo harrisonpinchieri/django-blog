1c18e84147358f87744b19c97f65398875561096
harrison <harrisonpinchieri@gmail.com>
2024-01-10 04:00:42 -0300

    frontend login-logout token exemplo

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,23 @@
 
+let loginBtn = document.getElementById('login-btn')
+let logoutBtn = document.getElementById('logout-btn')
+
+let token = localStorage.getItem('token')
+
+if (token) {
+    loginBtn.remove()
+} else {
+    logoutBtn.remove()
+}
+
+
+logoutBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    localStorage.removeItem('token')
+    window.location = 'file:///D:/ESTUDOS/django/udemy/devsearch/frontend/login.html'
+})
+
+
 let projectsUrl = 'http://127.0.0.1:8000/api/projects/'
 
 let getProjects = () => {
